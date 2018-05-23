308afbcd3d048373694f5e33b7025e61d10a6033
wuzhenzhou <greatzzwu@gmail.com>
2018-05-23 16:32:59 +0800

    finish demo core

--- src/jim_chain/core.clj
+++ src/jim_chain/core.clj
@@ -7,7 +7,12 @@
 ;;所有（其它）节点
 (def nodes (atom []))
 
-(defn hash
+(defn add-node!
+  "添加节点"
+  [address]
+  (swap! nodes conj address))
+
+(defn block-hash
   "计算区块的hash值"
   [block]
   (let [{:keys [index timestamp transactions previous-hash]}  block]
@@ -20,8 +25,12 @@
   {:index 0
    :timestamp 0
    :transactions []
-   :hash (hash genesis-block)
-   :previous-hash 0})
+   :hash (block-hash genesis-block)
+   :previous-hash 0
+   :proof 68867})
+
+;;chaindata
+(def chain (atom [genesis-block]))
 
 ;;提交到本节点，等待被写入的交易列表
 (def waitting-transactions (atom []))
@@ -32,3 +41,45 @@
   (swap! waitting-transactions conj {:sender sender
                                      :recipient recipient
                                      :amount amount}))
+
+
+;;##############POW###############
+(defn valid-proof?
+  "是否可用的工作量证明值"
+  [previous-block proof]
+  (.startsWith (digest/sha-256 (str (:hash previous-block) proof)) "0000"))
+
+(defn calculate-proof
+  "计算工作量证明值"
+  [previous-block]
+  (loop [p 0]
+    (if-not (valid-proof? previous-block p)
+      (recur (inc p))
+      p)))
+(comment
+  (proof-of-work genesis-block))
+
+(defn add-block!
+  [proof]
+  (let [previous-block (last @chain)
+        block {:index (inc (:index previous-block))
+               :timestamp (System/nanoTime)
+               :transactions @waitting-transactions
+               :previous-hash (:hash previous-block)
+               :proof proof}
+        block (assoc block :hash (block-hash block))]
+    (swap! chain conj block)
+    (reset! waitting-transactions [])))
+
+(defn mine!
+  []
+  (let [proof (calculate-proof (last @chain))]
+    ;;矿工奖励
+    (add-transaction! nil curr-node-addr 1)
+    (add-block! proof)))
+
+(defn sync-chain!
+  "和所有节点同步数据，找到最长的正确的链"
+  []
+  ;;TODO
+  )
